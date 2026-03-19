bd7cb3a9d708c0c9ca7ee64a206dae3aac693375
Mehedi H Sajib <sajibmehedi7@gmail.com>
2026-03-19 11:59:02 +0600

    mobile responsive

--- app/components/NavExtra.jsx
+++ app/components/NavExtra.jsx
@@ -1,26 +1,32 @@
 "use client";
 
+import AskAIButton from "./AskAIButton";
+import MobileNavMenu from "./MobileNavMenu";
+import SearchTriggerButton from "./SearchTriggerButton";
 import ThemeToggle from "./ThemeToggle";
 
 export default function NavExtra() {
   return (
     <div className="nav-extra">
-      {/* Ask AI button */}
-      {/* <button className="nav-ask-ai-btn">✦ Ask AI</button> */}
+      <div className="nav-extra-desktop">
+        <a href="#" className="nav-link">
+          Status
+        </a>
+        <a href="#" className="nav-link">
+          Blog
+        </a>
+        <a href="#" className="nav-link">
+          Support
+        </a>
+        <ThemeToggle size={16} />
+      </div>
 
-      {/* Nav links */}
-      <a href="#" className="nav-link">
-        Status
-      </a>
-      <a href="#" className="nav-link">
-        Blog
-      </a>
-      <a href="#" className="nav-link">
-        Support
-      </a>
-
-      {/* Theme toggle */}
-      <ThemeToggle size={16} />
+      <div className="nav-extra-mobile">
+        {/* <ThemeToggle size={16} /> */}
+        <SearchTriggerButton mobile />
+        <AskAIButton mobile />
+        <MobileNavMenu />
+      </div>
     </div>
   );
 }
